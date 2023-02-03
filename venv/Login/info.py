8b4a6186c42290dc2e5c4e985365fb74d0afd165
MikhailBaldaev <105664613+MikhailBaldaev@users.noreply.github.com>
2023-02-03 15:47:02 +0300

    Update info.py

--- venv/Login/info.py
+++ venv/Login/info.py
@@ -1,8 +1,8 @@
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smpt.gmail.com'
-EMAIL_HOST_USER = 'm53961589@gmail.com'
-EMAIL_HOST_PASSWORD = r'1qaz"WSX'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_USE_TLS = True
